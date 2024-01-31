7a85940a73dde9a3360e8d82f1cd33e98f5f4c50
Arjun Sankar <157756352+arjunsankar108@users.noreply.github.com>
2024-01-31 18:47:56 +0000

    Update geo.py

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -9,6 +9,7 @@ import math
 from .utils import sorted_by_key
 
 def haversin(coordA, coordB):
+    '''Returns the arc length between two points on the earth's surface for the great circle joining the two points.'''
     #convert degrees to radians
     lat1 = math.radians(coordA[0])
     lat2 = math.radians(coordB[0])
@@ -21,17 +22,21 @@ def haversin(coordA, coordB):
 
 
 def stations_by_distance(stations, p):
+    '''Returns a list of tuples with stations and their distances from the point p in ascending order of distance.'''
     return sorted_by_key( [ (station , haversin(station.coord,p)) for station in stations ], 1)
 
 def stations_within_radius(stations, centre, r):
+    '''Returns a list of stations within a distance r from the centre.'''
     return [station for station in stations if haversin(station.coord, centre) <= r]
 
 
 def rivers_with_station(stations):
+    '''Returns a list of rivers with at one or more monitoring stations.'''
     rivers = [station.river for station in stations if station.river is not None]
     return sorted(set(rivers))
 
 def stations_by_river(stations):
+    '''Returns a dictionary where keys correspond to rivers and values to lists of stations on that river.'''
     rivers = rivers_with_station(stations)
     river_stations = {river: [] for river in rivers}
     for station in stations:
@@ -41,6 +46,7 @@ def stations_by_river(stations):
 
 
 def rivers_by_station_number(stations, N):
+    '''Returns a list of tuples with rivers and their numbers of stations, sorted in descending order of the number of stations of each river.'''
     river_stations = stations_by_river(stations)
     river_num_stations = [(river, len(stations)) for river, stations in river_stations.items()]
     river_num_stations = sorted_by_key(river_num_stations, 1, reverse=True)
