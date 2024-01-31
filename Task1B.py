3e2d96be56decc1e903ad16c301c59e9024f65cc
Bailen O'Farrell <59749194+sheerlogic218@users.noreply.github.com>
2024-01-31 18:46:08 +0000

    Update Task1B.py

--- Task1B.py
+++ Task1B.py
@@ -7,23 +7,27 @@ def main():
     # Build list of stations
     stations = build_station_list()
 
-    #find the 10 closest stations to Cambridge
     p = (52.2053, 0.1218)
     stations_distance = stations_by_distance(stations, p)
     print("The 10 closest stations to Cambridge are: \n")
-    for station in stations_distance[:10]:
-        print("Station name: ", station[0].name+
-               ", Town: ", station[0].town,
-                "\nDistance: ", station[1]
-                ,"\n")
+    #prints nicely
+    # for station in stations_distance[:10]:
+    #     print("Station name: ", station[0].name+
+    #            ", Town: ", station[0].town,
+    #             "\nDistance: ", station[1]
+    #             ,"\n")
+    closest_10 = [(station[0].name, station[0].town, station[1]) for station in stations_distance[:10]]
+    print(closest_10)
     
     print("\n\nThe 10 furthest stations to Cambridge are: \n")
+    #prints nicely
     for station in stations_distance[-10:]:
         print("Station name: ", station[0].name+
                ", Town: ", station[0].town,
                 "\nDistance: ", station[1]
                 ,"\n")
-
+    
+    furthest_10 = stations_distance[-10:]
 
 
 if __name__ == "__main__":
