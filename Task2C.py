2ffd789fcb5ef0e3b1ebb46c1b2be4d7547709b1
Bailen O'Farrell <59749194+sheerlogic218@users.noreply.github.com>
2024-02-07 15:21:00 +0000

    Task2C

--- Task2C.py
+++ Task2C.py
@@ -1,11 +1,12 @@
 from floodsystem.flood import stations_highest_rel_level
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
 
 
 def main():
     # Build list of stations
     stations = build_station_list()
-
+    # Update water levels
+    update_water_levels(stations)
     # Find and print the 5 stations with the highest relative water levels
     N = 5
     print("5 stations with the highest relative water levels:")
