07a7def88719d11b56f31c9a980e2ad9695688fd
Bailen O'Farrell <59749194+sheerlogic218@users.noreply.github.com>
2024-02-07 15:12:33 +0000

    fix bad code

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -5,10 +5,10 @@ from .station import MonitoringStation
 
 def stations_level_over_threshold(stations, tol):
     '''Returns list of stations with water levels over the threshold'''
-    tol_stations = [(station, station.relative_water_level) for station in stations if station.relative_water_level() is not None and station.relative_water_level() > tol]
+    tol_stations = [(station, station.relative_water_level()) for station in stations if (station.relative_water_level() is not None and station.relative_water_level() > tol)]
     return sorted_by_key(tol_stations, 1, reverse=True)
 
 def stations_highest_rel_level(stations, N):
     '''Returns list of N stations with the highest relative water levels'''
-    rel_stations = [(station, station.relative_water_level) for station in stations if station.relative_water_level() is not None]
+    rel_stations = [(station, station.relative_water_level()) for station in stations if station.relative_water_level() is not None]
     return sorted_by_key(rel_stations, 1, reverse=True)[:N]
--- test_flood.py
+++ test_flood.py
@@ -1,12 +1,19 @@
 from floodsystem.flood import stations_level_over_threshold
 import floodsystem.station
+from floodsystem.stationdata import update_water_levels
 
 
-sample_stations = [floodsystem.station.MonitoringStation("1", "None", "A", (1, 1), 2, "river", None),
-                   floodsystem.station.MonitoringStation("2", "None", "B", (3, 3), 2, "river", None),
-                   floodsystem.station.MonitoringStation("3", "None", "C", (2, 2), 2, "river2", None)
+sample_stations = [floodsystem.station.MonitoringStation("1", "None", "A", (1, 1), [0,2], "river", None),
+                   floodsystem.station.MonitoringStation("2", "None", "B", (3, 3), [0,2], "river", None),
+                   floodsystem.station.MonitoringStation("3", "None", "C", (2, 2), [0,2], "river2", None)
                    ]
+sample_stations[0].latest_level = 1.8
+sample_stations[1].latest_level = 0.9
+sample_stations[2].latest_level = 0.1
 
 
 def test_stations_level_over_threshold():
-    tol = 0.5
+    assert stations_level_over_threshold(sample_stations, tol = 0.5)[0][0] == sample_stations[0]
+
+
+
